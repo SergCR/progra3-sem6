031c613f5f75b5f36edf0139a710d36a30a23121
Sergio Aguero <sergioaguerovalle@gmail.com>
2023-10-17 18:47:03 -0600

    SergioAguero

---
+++ practicasemana6/src/main/java/com/practicasemana6/practicasemana6/Practicasemana6Application.java
@@ -0,0 +1,13 @@
+package com.practicasemana6.practicasemana6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicasemana6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practicasemana6Application.class, args);
+	}
+
+}
---
+++ practicasemana6/src/test/java/com/practicasemana6/practicasemana6/Practicasemana6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicasemana6.practicasemana6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practicasemana6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
